45ffed213227038e2e00c84e27248478dde45e4c
gbrllim <gbrllim@gmail.com>
2023-11-30 19:50:32 +0800

    Fix (coins & config): amended env input and changed coins associations

--- db/models/index.js
+++ db/models/index.js
@@ -1,19 +1,16 @@
 "use strict";
 
-//importing what we need & initializes variables
 const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
 const process = require("process");
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || "production";
+const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../../config/database.js")[env];
 const db = {};
 
-// Develops the connection into the DB
 let sequelize;
 
-require("dotenv").config();
 if (process.env.DATABASE_URL) {
   sequelize = new Sequelize(
     process.env.DATABASE,
@@ -39,22 +36,19 @@ fs.readdirSync(__dirname)
   .filter((file) => {
     return (
       file.indexOf(".") !== 0 &&
-      file !== basename && // basename === 'index.js'
+      file !== basename &&
       file.slice(-3) === ".js" &&
       file.indexOf(".test.js") === -1
     );
   })
   .forEach((file) => {
-    //importing and initializing each model in our "models" directory
     const model = require(path.join(__dirname, file))(
       sequelize,
       Sequelize.DataTypes
     );
-    // storing the model into the empty db object
     db[model.name] = model;
   });
 
-// taking each model, and creating the associations
 Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
