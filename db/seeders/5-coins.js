6d6726f98d458f5bf9a8da8e0627a42f3589b1c0
shanquanSQ <thiashanquan93@gmail.com>
2023-12-09 21:06:12 +0800

    deposit and withdraw added

--- db/seeders/5-coins.js
+++ db/seeders/5-coins.js
@@ -5,7 +5,7 @@ module.exports = {
   async up(queryInterface) {
     await queryInterface.bulkInsert("coins", [
       {
-        coin_symbol: "wBTC",
+        coin_symbol: "WBTC",
         coin_name: "Wrapped Bitcoin",
         created_at: new Date(),
         updated_at: new Date(),
@@ -17,7 +17,7 @@ module.exports = {
         updated_at: new Date(),
       },
       {
-        coin_symbol: "wETH",
+        coin_symbol: "WETH",
         coin_name: "Wrapped Ethereum",
         created_at: new Date(),
         updated_at: new Date(),
@@ -25,6 +25,24 @@ module.exports = {
       {
         coin_symbol: "ETH",
         coin_name: "Ethereum",
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        coin_symbol: "sepoliaWBTC",
+        coin_name: "Sepolia WBTC",
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        coin_symbol: "sepoliaWETH",
+        coin_name: "Sepolia WETH",
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        coin_symbol: "sepoliaUSDC",
+        coin_name: "Sepolia USDC",
         created_at: new Date(),
         updated_at: new Date(),
       },
