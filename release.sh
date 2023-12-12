7e1d64d5c215df6194bcfd021c2743e06cba5891
gbrllim <gbrllim@gmail.com>
2023-12-12 12:22:12 +0800

    Release.sh add + new DB name

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
-# npx sequelize db:drop
-# npx sequelize db:create
-# npx sequelize db:migrate
-# npx sequelize db:seed:all
+npx sequelize db:drop
+npx sequelize db:create
+npx sequelize db:migrate
+npx sequelize db:seed:all
